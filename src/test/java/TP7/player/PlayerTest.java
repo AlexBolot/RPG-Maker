fdb24cea091922c58c5ab134af246bbbf7bcd4dc
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2019-12-27 18:45:08 +0100

    minor update - update code style

--- src/test/java/TP7/player/PlayerTest.java
+++ src/test/java/TP7/player/PlayerTest.java
@@ -3,8 +3,8 @@ package TP7.player;
 import TP7.entity.groups.Army;
 import TP7.entity.groups.Battalion;
 import TP7.entity.groups.Group;
+import TP7.entity.personnages.Character;
 import TP7.entity.personnages.Human;
-import TP7.entity.personnages.Personnage;
 import TP7.entity.personnages.Troll;
 import TP7.factories.GroupFactory;
 import TP7.factories.PersonnageFactory;
@@ -19,42 +19,42 @@ import static org.junit.Assert.assertEquals;
  .
  . The PlayerTest	 Class was Coded by : Alexandre BOLOT
  .
- . Last Modified : 27/12/2019 18:39
+ . Last Modified : 27/12/2019 18:45
  .
  . Contact : bolotalex06@gmail.com
  ...............................................................................................................................*/
 
 public class PlayerTest {
     private Player player = new Player("Player1", 5000);
-    
+
     @Test
     public void getName() {
         assertEquals("Player1", player.getName());
     }
-    
+
     @Test
     public void Player() throws Exception {
         Player player2 = (Player) player.clone();
 
         assertEquals(player, player2);
     }
-    
+
     @Test
     public void getMoneyCount() {
         assertEquals(5000, player.getMoneyCount(), 0.001);
     }
-    
+
     @Test
     public void pay() {
         player.pay(50);
-    
+
         assertEquals(4950, player.getMoneyCount(), 0.001);
-        
+
         player.pay(45);
-    
+
         assertEquals(4905, player.getMoneyCount(), 0.001);
     }
-    
+
     @Test
     public void addEntities() {
         Character p1 = PersonnageFactory.createPersonnage(HUMAN, "human1", "bleu", player);
@@ -62,20 +62,20 @@ public class PlayerTest {
         Character p3 = PersonnageFactory.createPersonnage(HUMAN, "human2", "gris", player);
         Army g1 = GroupFactory.createArmy("army1", TAUREN, "Pawndo", 10.4f, player);
         Group g2 = GroupFactory.creatGroup(BATTALION, "group1", TAUREN, 5, 10.4f, player);
-    
+
         g1.addBattalion((Battalion) g2);
-    
+
         player.addEntities(p1, p2, p3, g1);
-    
+
         assertEquals(4, player.getEntities().size());
     }
-    
+
     @Test
     public void getEntities() {
         player.addEntities(new Human("human1", "bleu"));
         player.addEntities(new Troll("troll1", "1664"));
         player.addEntities(new Human("human2", "gris"));
-    
+
         assertEquals(3, player.getEntities().size());
     }
     
